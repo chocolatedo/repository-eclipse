8aa8e8820e168206cb3c1540b96ddb1f143cad8a
do <15233267521@139.com>
2018-09-18 15:12:27 +0800

    spring_demo05_entity

---
+++ springboot_demo05_entity/src/main/java/com/tt/SpringbootDemo05EntityApplication.java
@@ -0,0 +1,12 @@
+package com.tt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo05EntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemo05EntityApplication.class, args);
+	}
+}
