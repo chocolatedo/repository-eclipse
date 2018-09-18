4889cd163379f25ac5249ed97360a9a31af5c2dd
do <15233267521@139.com>
2018-09-18 14:42:07 +0800

    测试

---
+++ springboot_demo07_resttemplate/src/test/java/com/tt/SpringbootDemo07ResttemplateApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=SpringbootDemo07ResttemplateApplication.class)
+public class SpringbootDemo07ResttemplateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("123");
+	}
+
+}
